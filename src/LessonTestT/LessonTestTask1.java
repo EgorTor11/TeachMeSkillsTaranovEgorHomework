29598119972d6c33aab8c8445c9deafaaefc6595
EgorTor11 <taranovegor91@gmail.com>
2021-10-10 23:23:18 +0300

    внес изменения

--- src/LessonTestT/LessonTestTask1.java
+++ src/LessonTestT/LessonTestTask1.java
@@ -2,7 +2,7 @@ package LessonTestT;
 
 public class LessonTestTask1 {
     public static void main(String[] args) {
-        System.out.println("Рамштайн переводится как таран(стенобитное устройство)");
+        System.out.println("Какието изменения");
     }
 
 }
